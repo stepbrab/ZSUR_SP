13579ce70d52cdb5a51d68f3165f8d402d7bc4fc
stepbrab <stepbrab@students.zcu.cz>
2023-03-22 09:10:42 +0100

    stepbrab

--- lin_disc_func.py
+++ lin_disc_func.py
@@ -1,48 +1,40 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
+def rosenblatt(train_X, train_y, max_iter=1000):
+    # Inicializujeme váhy na nulu
+    w = np.zeros(train_X.shape[1])
+    # Provedeme až max_iter iterací
+    for i in range(max_iter):
+        # Projdeme všechny trénovací příklady
+        for j in range(train_X.shape[0]):
+            # Spočteme výstup klasifikační funkce
+            output = np.dot(train_X[j], w)
+            # Pokud byl klasifikační výstup nesprávný, aktualizujeme váhy
+            if np.sign(output) != train_y[j]:
+                w += train_y[j] * train_X[j]
+        # Pokud jsou všechny trénovací příklady klasifikovány správně, ukončíme učení
+        if np.all(np.sign(np.dot(train_X, w)) == train_y):
+            break
+    return w
 
-def rosenblatt_classifier(X, y, learning_rate=0.1, max_epochs=1000, bias=True):
-    n_samples, n_features = X.shape
+def constant_increment(train_X, train_y, alpha=0.1, max_iter=1000):
+    # Inicializujeme váhy na nulu
+    w = np.zeros(train_X.shape[1])
+    # Provedeme až max_iter iterací
+    for i in range(max_iter):
+        # Projdeme všechny trénovací příklady
+        for j in range(train_X.shape[0]):
+            # Spočteme výstup klasifikační funkce
+            output = np.dot(train_X[j], w)
+            # Aktualizujeme váhy na základě konstanty učení a nesprávné klasifikace
+            if np.sign(output) != train_y[j]:
+                w += alpha * train_y[j] * train_X[j]
+    return w
 
-    # Initialize weights
-    weights = np.zeros(n_features + int(bias))
+#Tyto implementace předpokládají, že train_X je matice trénovacích příkladů, train_y je vektor klasifikačních cílů, alpha je konstanta učení a max_iter je maximální počet iterací, které mají být provedeny. Funkce vracejí váhy lineární diskriminační funkce pro daný trénovací dataset.
 
-    # Add bias term if necessary
-    if bias:
-        X = np.concatenate((X, np.ones((n_samples, 1))), axis=1)
-
-    # Train the model
-    for epoch in range(max_epochs):
-        for i in range(n_samples):
-            activation = np.dot(X[i], weights)
-            if activation >= 0.0:
-                y_pred = 1
-            else:
-                y_pred = -1
-            error = y[i] - y_pred
-            weights += learning_rate * error * X[i]
-
-    # Define the predict function
-    def predict(X_new):
-        if bias:
-            X_new = np.concatenate((X_new, np.ones((X_new.shape[0], 1))), axis=1)
-        return np.where(np.dot(X_new, weights) >= 0.0, 1, -1)
-
-        # Plot the data and the decision boundary
-        x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
-        y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
-        xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1))
-        Z = predict(np.c_[xx.ravel(), yy.ravel()])
-        Z = Z.reshape(xx.shape)
-        plt.figure(figsize=(8, 8))
-        plt.contourf(xx, yy, Z, alpha=0.4)
-        plt.scatter(X[:, 0], X[:, 1], c=y, s=20, edgecolors='k')
-        plt.xlim(xx.min(), xx.max())
-        plt.ylim(yy.min(), yy.max())
-        plt.xlabel('Feature 1')
-        plt.ylabel('Feature 2')
-        plt.title('Rosenblatt Classifier')
-        plt.show()
-
-    return weights
+#Klasifikátor s lineárními diskriminačními funkcemi slouží k rozdělení dat do dvou tříd na základě jejich lineární separability. Rosenblattův algoritmus a upravená metoda konstantních přírůstků jsou dva způsoby učení tohoto klasifikátoru.
+#Rosenblattův algoritmus je iterativní algoritmus, který aktualizuje váhy lineární diskriminační funkce po každém předložení trénovacího příkladu. Algoritmus se ukončí, když jsou všechny trénovací příklady klasifikovány správně, nebo když je dosaženo maximálního počtu iterací.
+#Upravená metoda konstantních přírůstků je také iterativní algoritmus, který aktualizuje váhy lineární diskriminační funkce po každém předložení trénovacího příkladu, ale používá konstantního kroku učení namísto adaptivního kroku, který se používá v Rosenblattově algoritmu.
+#Potřebný počet iterací závisí na mnoha faktorech, jako jsou složitost dat, velikost trénovací sady, volba konstanty učení a počátečních vah. Obecně platí, že Rosenblattův algoritmus má tendenci se učit rychleji než upravená metoda konstantních přírůstků, ale může být náchylnější k oscilacím a nemusí být schopen konvergovat v některých případech.
+#Pro zvolené konstanty učení by bylo třeba vyzkoušet oba algoritmy na trénovacích datech a porovnat počet iterací, které jsou potřebné pro dosažení určité úrovně přesnosti klasifikace. Obecně platí, že volba optimální konstanty učení závisí na konkrétních datech a musí být experimentálně stanovena.
